991ddb7bb899a6f236e843ef61d1c0f86f977123
Eric <eric.yanez50@qmail.cuny.edu>
2022-08-18 08:30:37 -0400

    commit tree 9489713528cd04b39ab8046c785ce417ac6a886c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end 
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif
+      a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero?
+  end
+
+  class TriangleError < StandardError
+  end
 end
